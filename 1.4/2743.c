ca677b77e6b187e84b64da8d6ec8f742b85a28cf
abbiddo <79645200+abbiddo@users.noreply.github.com>
2022-01-05 00:00:11 +0900

    srting length

---
+++ 1.4/2743.c
@@ -0,0 +1,19 @@
+/**
+프로그램명 : 2743.c
+설명 : 문자열 길이 
+작성일시 : 2022.01.04
+작성자 : 정소영
+**/
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char a[100];
+	scanf("%s",a);
+	printf("%d",strlen(a));
+}
+
+/**
+pulljima
+8
+**/ 
