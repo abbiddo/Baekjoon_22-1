953ce0ece6075dc63ccd68165f6073dd999e431a
abbiddo <79645200+abbiddo@users.noreply.github.com>
2022-01-09 19:36:49 +0900

    cmd string
    문자열은 범위 크기 +1 ( 50 -> 51 )

---
+++ 1.9/1032.c
@@ -0,0 +1,34 @@
+/**
+프로그램명 : 1032.c
+설명 : 명령 프롬프트 
+작성일시 : 2022.01.09
+작성자 : 정소영
+**/
+
+#include <stdio.h>
+
+int main(){
+	char word[51][51];
+	int n,i,j;
+	
+	scanf("%d",&n);
+	for (i=0;i<n;i++) scanf("%s",word[i]);
+	
+	for (i=0;i<strlen(word);i++)
+		for (j=0;j<n-1;j++)
+			if (word[j][i]!=word[j+1][i]){
+				word[0][i]='?';
+				break;
+			}
+	
+	printf("%s",word[0]);
+}
+
+/**
+실행 결과
+3            (입력) 
+config.sys
+config.inf
+configures
+config????   (출력) 
+**/ 
