fdf95c5efbf5d7d4824378dd76099c945cff0714
abbiddo <79645200+abbiddo@users.noreply.github.com>
2022-01-11 19:17:21 +0900

    fibonacci
    오류 해결 (재귀 함수 -> 반복문)

--- 01.11/1003.c
+++ 01.11/1003.c
@@ -6,35 +6,23 @@
 **/
 
 #include <stdio.h>
-
-int n0=0;
-int n1=0;
-
-int fibo(int n) {
-    if (n == 0) {
-        //printf("0");
-        n0++;
-        return 0;
-    } 
-	
-	else if (n == 1) {
-        //printf("1");
-        n1++;
-        return 1;
-    } 
-    
-	else return fibo(n-1) + fibo(n-2);
-}
-
 int main(){
-	int n,i,a[40];
+	int n,i,j,k,kk,re,a[100];
 	scanf("%d",&n);
+	
 	for (i=0;i<n;i++){
-		n0=0;
-		n1=0;
 		scanf("%d",&a[i]);
-		fibo(a[i]);
-		printf("%d %d\n",n0,n1);
+		
+		k=1;
+		kk=re=0;
+		
+		for (j=0;j<a[i];j++){
+			re=k+kk;
+			k=kk;
+			kk=re;
+		}
+		
+		printf("%d %d\n",k,kk);
 	} 
 }
 
