0e24d520f762a5de01f66fcf7dd0c5bff3d9658f
abbiddo <79645200+abbiddo@users.noreply.github.com>
2022-02-02 03:32:30 +0900

    QE Padovan squence
    혼자 알고리즘을 찾았으나 반례를 모르겠음

---
+++ 02.01/9462.cpp
@@ -0,0 +1,30 @@
+/**
+프로그램명 : 9461.cpp
+설명 : 파도반 수열 
+작성일시 : 2022.02.01
+작성자 : 정소영 
+**/
+
+#include <iostream>
+using namespace std;
+int main(){
+	long long p[101]={0,1,1,1,2,2,3,4};
+	int n,k,j=8;
+	cin>>n;
+
+	for (int i=0;i<n;i++){
+		cin>>k;
+		if (k>=j) 
+			for (int l=j;l<=k;l++) p[j++]=p[j-2]+p[j-6];
+		cout<<p[k]<<'\n';
+	}
+}
+
+/**
+2
+6
+3
+12
+16
+
+**/ 
