dbd23ecccf86ef0e6472d55f9f48441ddad802b9
abbiddo <79645200+abbiddo@users.noreply.github.com>
2022-02-22 16:36:48 +0900

    M tomato
    재귀는 시간 복잡도가 큼
    게시판에서 1만 찾으라는 힌트를 보고 풀었음

--- 02.22/7576.cpp
+++ 02.22/7576.cpp
@@ -8,40 +8,53 @@
 #include <iostream>
 using namespace std;
 int arr[1000][1000];
-int n,m,k,zcnt=0;
+int num[1000000][2];
+int num2[1000000][2];
+int n,m,k=1,c=0,cc=0,day=0,zcnt=0;
 
 int change(int i, int j){
-	if ((arr[i+1][j]==0 || arr[i+1][j]>arr[i][j]) && i<n-1) {
-		if (arr[i+1][j]==0) zcnt--;
-		arr[i+1][j]=arr[i][j]+1;
-		change(i+1,j);
+	arr[i][j]=-1;
+	if (arr[i+1][j]==0 && i<n-1) {
+		arr[i+1][j]=1;
+		num[cc][0]=i+1;
+		num[cc][1]=j;
+		cc++;
+		zcnt--;
 	}
-	if ((arr[i-1][j]==0 || arr[i-1][j]>arr[i][j]) && i>0) {
-		if (arr[i-1][j]==0) zcnt--;
-		arr[i-1][j]=arr[i][j]+1;
-		change(i-1,j);
+	if (arr[i-1][j]==0 && i>0) {
+		arr[i-1][j]=1;
+		num[cc][0]=i-1;
+		num[cc][1]=j;
+		cc++;
+		zcnt--;
 	}
-	if ((arr[i][j+1]==0 || arr[i][j+1]>arr[i][j]) && j<m-1) {
-		if (arr[i][j+1]==0) zcnt--;
-		arr[i][j+1]=arr[i][j]+1;
-		change(i,j+1);
+	if (arr[i][j+1]==0 && j<m-1) {
+		arr[i][j+1]=1;
+		num[cc][0]=i;
+		num[cc][1]=j+1;
+		cc++;
+		zcnt--;
 	}
-	if ((arr[i][j-1]==0 || arr[i][j-1]>arr[i][j]) && j>0) {
-		if (arr[i][j-1]==0) zcnt--;
-		arr[i][j-1]=arr[i][j]+1;
-		change(i,j-1);
+	if (arr[i][j-1]==0 && j>0) {
+		arr[i][j-1]=1;
+		num[cc][0]=i;
+		num[cc][1]=j-1;
+		cc++;
+		zcnt--;
 	}
 	return 0;
 }
 
 int tomato(){
-	for (int i=0;i<n;i++){
-		for (int j=0;j<m;j++){
-			if (arr[i][j]==1){
-				change(i,j);
-			}
-		}
+	for (int i=0;i<c;i++)
+		change(num2[i][0],num2[i][1]);
+		
+	for (int i=0;i<cc;i++){
+		num2[i][0]=num[i][0];
+		num2[i][1]=num[i][1];
 	}
+		
+	day++;
 	return 0;
 }
 
@@ -54,16 +67,24 @@ int main(){
 		for (int j=0;j<m;j++) {
 			cin>>arr[i][j];
 			if (arr[i][j]==0) zcnt++;
+			else if (arr[i][j]==1){
+				num2[c][0]=i;
+				num2[c][1]=j;
+				c++;
+			}
 		}
 		
-	tomato();
-	
-	for (int i=0;i<n;i++)
-		for (int j=0;j<m;j++)
-			if (k<arr[i][j]) k=arr[i][j];
-	
-	if (zcnt!=0) cout<<-1;
-	else cout<<k-1;
+	for (int i=0;i<n*m/3*2;i++)	{
+		if (zcnt==0){
+			cout<<day;
+			
+			return 0;
+		}
+		tomato();
+		c=cc;
+		cc=0;
+	}
+	cout<<-1;
 }
 
 /**
