2af8679ff8a25d46eaf8f3a7ddba0b8acc4b7cc1
abbiddo <79645200+abbiddo@users.noreply.github.com>
2022-03-10 15:22:51 +0900

    N&M 12

---
+++ 15666.cpp
@@ -0,0 +1,53 @@
+/**
+프로그램명 : 15666.cpp
+설명 : N과 M 12
+작성일시 : 2022.03.10
+작성자 : 정소영 
+**/
+
+#include <iostream>
+using namespace std;
+
+int n,m;
+int arr[8];
+int v[10001];
+
+void dfs(int num, int cnt){
+	if (cnt==m){
+		for (int i=0;i<m;i++) cout<<arr[i]<<" ";
+		cout<<"\n";
+		return;
+	}
+	for (int i=num;i<=10000;i++){
+		if (v[i]==1){
+			arr[cnt]=i;
+			dfs(i, cnt+1);
+		}
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int a;
+	cin>>n>>m;
+	for (int i=0;i<n;i++){
+		cin>>a;
+		v[a]=1;
+	}
+	dfs(1,0);
+}
+
+/**
+실행 결과 
+4 2
+9 7 9 1
+-----
+1 1
+1 7
+1 9
+7 7
+7 9
+9 9
+**/ 
